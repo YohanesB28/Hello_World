84b386417fbc9e2ee8a280fcc5350049a417b056
YohanesB28 <YohanesB28@users.noreply.github.com>
2020-07-14 11:05:32 +0700

    up

--- Latihan/src/LatWarmUp/no2.java
+++ Latihan/src/LatWarmUp/no2.java
@@ -12,13 +12,49 @@ package LatWarmUp;
 public class no2 {
 
     public static void main(String[] args) {
-        for (int x = 15; x < 30; x++) {
-            for (int i = 15; i >= 0; i--) {
+        int f = 0;
+        for (int x = 1; x <= 15; x++) {
+            for (int i = 15 - x; i >= 0; i--) {
                 System.out.print(" ");
-                for (int j = 0; j < 10; j++) {
-                    System.out.print("*");
+            }
+            for (int j = 1; j <= x + f; j++) {
+                switch (x) {
+                    case 4:
+                    case 7:
+                    case 10:
+                    case 13:
+                        if ((1 < j && j < 7) || (7 < j && j < 13) || (13 < j && j < 19) || (19 < j && j < 25)) {
+                            System.out.print(".");
+                        } else {
+                            System.out.print("*");
+                        }
+                        break;
+                    case 5:
+                    case 8:
+                    case 11:
+                    case 14:
+                        if ((3 < j && j < 7) || (9 < j && j < 13) || (15 < j && j < 19) || (21 < j && j < 25)) {
+                            System.out.print(".");
+                        } else {
+                            System.out.print("*");
+                        }
+                        break;
+                    case 6:
+                    case 9:
+                    case 12:
+                    case 15:
+                        if ((5 < j && j < 7) || (11 < j && j < 13) || (17<j && j<19) || (23<j && j<25)) {
+                            System.out.print(".");
+                        } else {
+                            System.out.print("*");
+                        }
+                        break;
+                    default:
+                        System.out.print("*");
+                        break;
                 }
             }
+            f++;
             System.out.println();
         }
     }
--- Latihan/src/LatWarmUp/no4.java
+++ Latihan/src/LatWarmUp/no4.java
@@ -12,13 +12,9 @@ package LatWarmUp;
 import java.util.Scanner;
 public class no4 {
     public static void main(String[] args) {
-        Scanner sh=new Scanner(System.in);
-        double insentifJasa, insentifKomisi, pajakIntensif, totalGaji;
-        System.out.print("Masukkan jumlah penjualan sales marketing = Rp.");
-        int jumlah =sh.nextInt();
-        if (jumlah<3000000) {
-            insentifJasa=0.015*jumlah;
-            insentifKomisi=0.25*insentifJasa;
-        }
+        int a=7;
+        int b=2;
+        int c=5%2;
+        System.out.println(c);
     }
 }
